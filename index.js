d10c1ddc250959d08af4f3e586a531e41abc3983
Asish George <asish.ag@iinerds.com>
2020-07-24 12:51:12 +0530

    Auth Changes

--- index.js
+++ index.js
@@ -14,4 +14,16 @@ app.get('/plans', (req, res) => {
     res.sendFile(path.join(__dirname, 'ui', 'path.html'));
 })
 
+
+
+app.get('/returnURL', (req, res) => {
+    res.send("This is the retrun URL.")
+})
+
+
+
+app.get('/bundle.js', (req, res) => {
+    res.sendFile(path.join(__dirname, 'ui', 'bundle.js'));
+})
+
 app.listen(process.env.PORT || 5000, console.log("Server started to listen on port " + process.env.PORT))
---
+++ main.js
@@ -0,0 +1,2 @@
+const jwt = require('jsonwebtoken');
+global.window.jwt = jwt;
